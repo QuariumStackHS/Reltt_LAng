0aa7357fa7ab770a432c521a1c448380690d2620
slowriding <48739803+slowriding@users.noreply.github.com>
2021-04-08 14:40:57 -0400

    VM update

--- Reltt/Modules/Qemu-D.cpp
+++ Reltt/Modules/Qemu-D.cpp
@@ -2,117 +2,170 @@
 #include "../includes/Mods.hpp"
 #include <time.h>
 #include <sstream>
-void*launch(Reltt_INT*IN){
-    string cmd="qemu-system-x86_64 ";
-    if(strcmp(getenv("Boot-drive"),""))
-    cmd.append(((string)"-hda ").append(IN->get_fileOBJ(getenv("Boot-drive")))).append(" ");
-    cmd.append("-cpu max ");
-    cmd.append(((string)"-smp ").append(getenv("cores")));
-    cmd.append(((string)" -m ").append((getenv("Mem"))).append("G"));
-    if(strcmp(getenv("install-drive"),""))
-    cmd.append(((string)" -cdrom ").append(IN->get_fileOBJ(getenv("install-drive"))));
-    cout<<cmd<<endl;
+string hdd;
+int hdds = 0;
+string cdrom;
+string Phdd;
+string sharedfolder;
+
+void *launch(Reltt_INT *IN)
+{
+    string cmd = "qemu-system-x86_64";
+    if (strcmp(hdd.c_str(), ""))
+        cmd.append(hdd);
+    cmd.append(" -cpu max");
+    cmd.append(sharedfolder);
+    cmd.append(((string) " -smp ").append(getenv("cores")));
+    cmd.append(((string) " -m ").append((getenv("Mem"))).append("G"));
+    if (strcmp(cdrom.c_str(), ""))
+        cmd.append(cdrom);
+    cout << cmd << endl;
     system(cmd.c_str());
-
 }
-void*Backup_VM(Reltt_INT*IN){
-  time_t rawtime;
-  struct tm * timeinfo;
-  char buffer [80];
+void *Add_shared_folder(Reltt_INT *IN)
+{
+    string acces=resolve_parentensis(IN)->S_value;
+    string Path=resolve_parentensis(IN)->S_value;
+    sharedfolder.append(" -drive file=fat:"+acces+":"+Path+",format=raw");
+}
+void *Backup_VM(Reltt_INT *IN)
+{
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
 
-  time (&rawtime);
-  timeinfo = localtime (&rawtime);
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
 
-  strftime (buffer,80,"%B-%d-%Y-%I-%M",timeinfo);
-    string cmd="qemu-img convert -p -f vpc -O qcow2 ";
-    cmd.append(getenv("Boot-drive"));
-    cmd.append(((string)" ")+buffer+(getenv("Boot-drive")));
+    strftime(buffer, 80, "%B-%d-%Y-%I-%M", timeinfo);
+    string cmd = "qemu-img convert -p -f vpc -O qcow2 ";
+    cmd.append(IN->get_fileOBJ(Phdd));
+    cmd.append(" ");
+    cmd.append(((string)getenv("backup_folder")) + Phdd + "-Snapshot-" + buffer);
     cmd.append(".qcow2");
-    cout<<cmd<<endl;
+    cout << cmd << endl;
     system(cmd.c_str());
+}
+void *create_HDD(Reltt_INT *IN)
+{
 
-}
-void*create_HDD(Reltt_INT*IN){
-    
-    string cmd="qemu-img create -f vpc ";
+    string cmd = "qemu-img create -f vpc ";
     cmd.append(resolve_parentensis(IN)->S_value);
     cmd.append(" ");
     cmd.append(resolve_parentensis(IN)->S_value);
-    cout<<cmd<<endl;;
+    cout << cmd << endl;
+    ;
     system(cmd.c_str());
 }
-void*Restore_VM(Reltt_INT*IN){
-    string cmd="qemu-img convert -p -f qcow2 -O vpc ";
-    cmd.append(getenv("Boot-drive"));
-    cmd.append(" ");
+void *Restore_VM(Reltt_INT *IN)
+{
+    string cmd = "qemu-img convert -p -f qcow2 -O vpc ";
     cmd.append(resolve_parentensis(IN)->S_value);
-    
-    cout<<cmd<<endl;
+    cmd.append(" ");
+
+    cmd.append(IN->get_fileOBJ(Phdd));
+    cout << cmd << endl;
     system(cmd.c_str());
 }
-void*Set_HDD(Reltt_INT*IN){
-    string iso=resolve_parentensis(IN)->S_value;
-    setenv("Boot-drive",iso.c_str(),1);
+void *Set_HDD(Reltt_INT *IN)
+{
+    string iso = resolve_parentensis(IN)->S_value;
+    string hd;
+    if (hdds == 0)
+    {
+        hd=" -hda ";
+        Phdd=iso;
+    }
+    elif (hdds == 1)
+    {
+        hd=" -hdb ";
+    }
+    elif (hdds == 2)
+    {
+        hd=" -hdc ";
+    }
+    elif (hdds == 3)
+    {
+        hd=" -hdd ";
+    }
+    hdds++;
+    hdd.append(hd);
+    hdd.append(IN->get_fileOBJ(iso));
+    
 }
-void*Add_iso(Reltt_INT*IN){
-    string iso=resolve_parentensis(IN)->S_value;
-    setenv("install-drive",iso.c_str(),1);
+void *Add_iso(Reltt_INT *IN)
+{
+    string iso = resolve_parentensis(IN)->S_value;
+    cdrom.append(" -cdrom");
+    cdrom.append(IN->get_fileOBJ(iso));
 }
-void*set_cores(Reltt_INT*IN){
-    string iso=resolve_parentensis(IN)->S_value;
-    setenv("cores",iso.c_str(),1);
+void *set_cores(Reltt_INT *IN)
+{
+    string iso = resolve_parentensis(IN)->S_value;
+    setenv("cores", iso.c_str(), 1);
 }
-void*set_Mem(Reltt_INT*IN){
-    string iso=resolve_parentensis(IN)->S_value;
-    setenv("Mem",iso.c_str(),1);
+void *set_Mem(Reltt_INT *IN)
+{
+    string iso = resolve_parentensis(IN)->S_value;
+    setenv("Mem", iso.c_str(), 1);
 }
-void*Create_script(Reltt_INT*IN){
-    string Vmname=resolve_parentensis(IN)->S_value;
-    string PATH=resolve_parentensis(IN)->S_value;
-    string size=resolve_parentensis(IN)->S_value;
-    string iso=resolve_parentensis(IN)->S_value;
-    string Mem=resolve_parentensis(IN)->S_value;
-    string Cores=resolve_parentensis(IN)->S_value;
+void *Create_script(Reltt_INT *IN)
+{
+    string Vmname = resolve_parentensis(IN)->S_value;
+    string PATH = resolve_parentensis(IN)->S_value;
+    string size = resolve_parentensis(IN)->S_value;
+    string iso = resolve_parentensis(IN)->S_value;
+    string Mem = resolve_parentensis(IN)->S_value;
+    string Cores = resolve_parentensis(IN)->S_value;
     stringstream ss;
-    ss<<"Begin:";
-
-ss<<"\n\nPATH "<<PATH<<"\n";
-ss<<"\n\nSet_HDD "<<Vmname<<".vhd\n";
-ss<<"\n\n<> ISO "<<iso<<"\n";
-ss<<"\n\ncores "<<Cores<<"\n";
-ss<<"\n\nMem "<<Mem<<"\nend;";
+    ss << "Begin:\n\n";
+    ss << "PATH " << PATH+Vmname << "/\n";
+    ss << "set_bcfolder " << PATH+"snapshot/" << "\n";
+    ss << "Set_HDD " << Vmname << ".vhd\n";
+    ss << "<> ISO " << iso << "\n";
+    ss << "cores " << Cores << "\n";
+    ss << "Mem " << Mem << "\nend;";
     ofstream myfile;
-    string fileout = PATH;
+
+    string fileout = getenv("RelttPath");fileout.append("VM/");
+    
     fileout.append(Vmname).append(".RlS");
-    myfile.open(fileout.c_str());
-    myfile<<ss.str();
+    cout<<fileout<<endl<<ss.str()<<endl;
+    myfile.open(fileout.c_str(),ios::app);
+    myfile << ss.str();
     myfile.close();
 
-    string cmd="qemu-img create -f vpc ";
-    cmd.append(PATH+Vmname+".vhd");
+    string cmd = "qemu-img create -f vpc ";
+    cmd.append(PATH + Vmname + ".vhd");
     cmd.append(" ");
     cmd.append(size);
     system(cmd.c_str());
-
-
+}
+void *bcfolder(Reltt_INT *IN)
+{
+    string iso = resolve_parentensis(IN)->S_value;
+    setenv("backup_folder", iso.c_str(), 1);
 }
 Reltt_INT::QSRcModule Qemu_D()
 {
     Reltt_INT::QSRcModule M;
-    setenv("Backup-Folder","NONE",0);
-    setenv("Boot-drive","NONE",0);
-    setenv("install-drive","",0);
-    setenv("Mem","6000",0);
-    setenv("cores","1",0);
+    setenv("Backup-Folder", "NONE", 0);
+    setenv("Boot-drive", "NONE", 0);
+    setenv("install-drive", "", 0);
+    setenv("Mem", "6000", 0);
+    setenv("cores", "1", 0);
+    setenv("backup_folder", "None", 0);
     M.Set_Name("qemu");
     M.add_Cask("launch", " run command build with no args", &launch);
     M.add_Cask("Set_HDD", "-> /Boot-drive", &Set_HDD);
     M.add_Cask("add_iso_boot", " -> /Iso.iso", &Add_iso);
+    M.add_Cask("set_bcfolder", " -> /Iso.iso", &bcfolder);
     M.add_Cask("cores", " -> int", &set_cores);
     M.add_Cask("Mem", " -> int", &set_Mem);
     M.add_Cask("Backup", " -> int", &Backup_VM);
     M.add_Cask("Restore", " -> Path_of_backup", &Restore_VM);
-    M.add_Cask("Create_Hdd"," disk_name size ",&create_HDD);
-    M.add_Cask("Create_script","-> VMname PATH size ISO MEM CORES ",&Create_script);
+    M.add_Cask("Create_Hdd", " disk_name size ", &create_HDD);
+    M.add_Cask("Create_script", "-> VMname PATH size ISO MEM CORES ", &Create_script);
+    M.add_Cask("Add_shared_folder","-> rw/r/w Path",&Add_shared_folder);
     return M;
 }
